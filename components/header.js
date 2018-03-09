a38ba0797417065bab1347883a56337123682988
Jon Heslop <jon@jonheslop.com>
2018-03-09 15:32:23 +0000

    Basic pull of contentful API to resolve #2

---
+++ components/header.js
@@ -0,0 +1,5 @@
+export default () => (
+	<header className="bg-near-black pa4">
+		<h1 className="ma0 near-white fw6">Dotwatchers.cc</h1>
+	</header>
+)
--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,49 @@
-export default () => <h1 className="f-headline">Hello, World!</h1>
+import React, { Component } from 'react';
+import {createClient} from 'contentful';
+
+import Header from '../components/header';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			posts: []
+		};
+	}
+
+	componentDidMount() {
+		const client = createClient({
+			space: '6hyijb95boju',
+			accessToken: 'f214dba82579af555cd4839172570328cf8aee10e37bf5b83094953bb65fb317'
+		});
+
+		client.getEntries()
+			.then(entries => {
+				console.log('entries', entries.items);
+				this.setState({
+					posts: entries.items
+				});
+			});
+	}
+
+	render() {
+		return (
+			<div>
+				<Header/>
+				<main className="ph4">
+					<h1 className="f-headline">Hello, World!</h1>
+					{
+						this.state.posts.map(item => (
+							<div key={item.sys.id}>
+								<h1>{item.fields.name}</h1>
+								<p className="lh-copy measure-wide">{item.fields.biography}</p>
+							</div>
+						))
+					}
+				</main>
+			</div>
+		);
+	}
+}
+
+export default App;
