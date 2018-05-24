3a4c5c0e5ca8d58242ffded5dce143031d8fda17
Jon Heslop <jon@jonheslop.com>
2018-05-24 08:33:38 +0100

    Fix header on scroll up

--- components/header/index.js
+++ components/header/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 import tachyons from 'styled-components-tachyons';
 import {Link} from '../../routes';
 import Logo from './logo';
 import slugify from '../../utils/slugify';
 
-const Header = styled.header`${tachyons}`;
 const A = styled.a`${tachyons}`;
 const Nav = styled.div`
 	margin-left: auto;
@@ -21,27 +20,82 @@ const Div = styled.div`
 	margin-left: var(--spacing-large)
 `;
 
-const Banner = ({title, raceName, race}) => (
-	<React.Fragment>
-		<Header bg_white bb b__light_gray near_black w_100 z_1 relative flex self_start className="cf">
-			<H1 flex items_center f2 pv3 ma0 fw5 lh_solid w_40_l>
-				<Div>
-					<Logo>{title}</Logo>
-				</Div>
-			</H1>
-			{ raceName ? <H2 dn pa3 flex_ns items_center f3 ma0 lh_solid fw5><Link route="race" params={{type: 'race', id: race.sys.id, raceID: race.fields.trackleadersRaceId, slug: slugify(race.fields.title)}} passHref prefetch><A no_underline near_black hover_blue>{race.fields.title}</A></Link></H2> : null}
-			<Nav ph4 pv3 lh_solid f2 flex_grow flex items_center>
-				<Link href="/" as="/" passHref prefetch>
-					<A dib mr3 f5 f4_l near_black fw4>Races</A>
-				</Link>
-				<Link route="page" params={{type: 'page', id: '1BgGLGEpckYcmoEE6Cqc0I'}} passHref>
-					<A dib ml3 f5 f4_l near_black fw4>About</A>
-				</Link>
-			</Nav>
-		</Header>
-		{ raceName ? <H2 tc pv2 dn_ns f4 ma0 lh_solid fw6 bt bw3 b__light_blue><Link route="race" params={{type: 'race', id: race.sys.id, raceID: race.fields.trackleadersRaceId, slug: slugify(race.fields.title)}} passHref prefetch><A no_underline near_black hover_blue>{race.fields.title}</A></Link></H2> : null}
-	</React.Fragment>
-);
+class Banner extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			bannerHeight: 0,
+			fixed: false,
+			width: 320,
+			lastScrollY: 0
+		};
+	}
+
+	componentDidMount() {
+		this.setState({ width: window.innerWidth });
+		this.setupStickyHeader()
+	}
+
+	componentWillUnmount() {
+		if (this.state.width > 1024) {
+			document.removeEventListener('scroll', this.handleScroll);
+		}
+	}
+
+	setupStickyHeader() {
+		document.addEventListener('scroll', this.headerPop.bind(this));
+		const banner = document.querySelector('#banner');
+		this.setState({
+			bannerHeight: banner.offsetHeight
+		});
+	}
+
+	headerPop() {
+		const {lastScrollY} = this.state;
+		const currentScrollY = window.scrollY;
+
+		if (currentScrollY < this.state.bannerHeight) {
+			this.setState({ fixed: false });
+		} else if (currentScrollY < lastScrollY) {
+				this.setState({ fixed: true });
+		} else {
+			this.setState({ fixed: false });
+		}
+		this.setState({ lastScrollY: currentScrollY });
+	}
+
+	render() {
+		const Header = styled.header`
+			position: relative;
+			top: 0;
+			${ props => props.isFixed && css`
+				position: fixed;
+			`}
+			${tachyons}`;
+		const padTop = this.state.fixed ? <Div style={{height: this.state.bannerHeight}}/> : null;
+		return (
+			<React.Fragment>
+				{padTop}
+				<Header bg_white bb b__light_gray near_black w_100 z_2 flex self_start className="cf" id="banner" isFixed={this.state.fixed}>
+					<H1 flex items_center f2 pv3 ma0 fw5 lh_solid w_40_l>
+						<Div>
+							<Logo>{this.props.title}</Logo>
+						</Div>
+					</H1>
+					{this.props.raceName ? <H2 dn pa3 flex_ns items_center f3 ma0 lh_solid fw5><Link route="race" params={{ type: 'race', id: this.props.race.sys.id, raceID: this.props.race.fields.trackleadersRaceId, slug: slugify(this.props.race.fields.title) }} passHref prefetch><A no_underline near_black hover_blue>{this.props.race.fields.title}</A></Link></H2> : null}
+					<Nav ph4 pv3 lh_solid f2 flex_grow flex items_center>
+						<Link href="/" as="/" passHref prefetch>
+							<A dib mr3 f5 f4_l near_black fw4>Races</A>
+						</Link>
+						<Link route="page" params={{type: 'page', id: '1BgGLGEpckYcmoEE6Cqc0I'}} passHref>
+							<A dib ml3 f5 f4_l near_black fw4>About</A>
+						</Link>
+					</Nav>
+				</Header>
+			</React.Fragment>
+		);
+	}
+}
 
 Banner.propTypes = {
 	title: PropTypes.string.isRequired,
--- pages/post.js
+++ pages/post.js
@@ -5,6 +5,8 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import tachyons from 'styled-components-tachyons';
 import Header from '../components/header';
+import {Link} from '../routes';
+import slugify from '../utils/slugify';
 import Page from '../components/shared/page';
 import Post from '../components/post';
 import {withEntries} from '../data/with-entries';
@@ -15,6 +17,7 @@ const PostWrapper = styled.div`
 	margin-left: 40%;
 }
 ${tachyons}`;
+const A = styled.a`${tachyons}`;
 
 class PostPage extends React.Component {
 	render() {
@@ -27,10 +30,14 @@ class PostPage extends React.Component {
 				</Head>
 				<Header
 					title="dotwatcher.cc"
+					raceName={this.props.posts[0].data.categories[0].fields.title}
 					race={this.props.posts[0].data.categories[0]}
 				/>
 				<MapContainer raceID={this.props.posts[0].data.categories[0].fields.trackleadersRaceId}/>
 				<PostWrapper fl w_50 pa4>
+					<Link route="race" params={{ type: 'race', id: this.props.posts[0].data.categories[0].sys.id, raceID: this.props.posts[0].data.categories[0].fields.trackleadersRaceId, slug: slugify(this.props.posts[0].data.categories[0].fields.title) }} passHref prefetch>
+						<A near_black f6 href="#">« Back to {this.props.posts[0].data.categories[0].fields.title} feed</A>
+					</Link>
 					<Post key={this.props.posts[0].sys.id} id={this.props.posts[0].sys.id} data={this.props.posts[0].data}/>
 				</PostWrapper>
 			</Page>
