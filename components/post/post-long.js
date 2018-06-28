b0a13fd7da9d39526c151b41a6f3126e82cf0d84
Jon Heslop <jon@jonheslop.com>
2018-06-28 11:32:58 +0100

    Added continue reading functionality
    Added logic to parse body for presence of [[ continue ]] short tag.
    
    If it’s present it splits the body and puts a continue reading button.

--- components/post/post-long.js
+++ components/post/post-long.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {Component} from 'react';
 import ReactMarkdown from 'react-markdown';
 import shortcodes from 'remark-shortcodes';
 import styled from 'styled-components';
@@ -28,21 +28,43 @@ const Div = styled.div`
 	}
 ${tachyons}`;
 const A = styled.a`${tachyons}`;
+const Button = styled.button`
+  border: 0;
+  padding: 0;
+  appearance: none;
+  background-color: transparent;
+${tachyons}`;
 
-const Long = ({data, id}) => {
-	const host = typeof window !== 'undefined' ? window.location.host : '';
+class Long extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			showMore: false
+		};
+		this.toggleMore = this.toggleMore.bind(this);
+	}
 
-	return (
-		<React.Fragment>
-			{ data.image ? <Image data={data.image.fields}/> : null }
-			<H1 f2 fw6 lh_title mt0>
-				<Link route="post" params={{type: 'post', id, slug: slugify(data.title)}} passHref prefetch>
-					<A link near_black hover_blue>{quotes(widont(data.title))}</A>
-				</Link>
-			</H1>
-			<Div lh_copy pb3>
+	render() {
+		const host = typeof window !== 'undefined' ? window.location.host : '';
+		const contineRegEx = RegExp(/\[\[\s?continue\s?\]\]/, 'gi');
+		const shouldSplitText = contineRegEx.test(this.props.data.body);
+		let splitText = null;
+		let continueReading = null;
+		let moreText = null;
+		let showLess = null;
+
+		if (shouldSplitText) {
+			const splitTextMarker = this.props.data.body.match(contineRegEx)[0];
+			splitText = this.props.data.body.split(splitTextMarker);
+			continueReading = (
+				<Button near_black hover_blue underline_hover onClick={this.toggleMore}>▼ Continue reading</Button>
+			);
+			showLess = (
+				<Button near_black hover_blue underline_hover mt3 onClick={this.toggleMore}>▲ Show less</Button>
+			);
+			moreText = this.state.showMore ? (
 				<ReactMarkdown
-					source={data.body}
+					source={splitText[1].trim()}
 					plugins={[shortcodes]}
 					escapeHtml={false}
 					renderers={{
@@ -50,10 +72,42 @@ const Long = ({data, id}) => {
 						image: BodyImage
 					}}
 				/>
-			</Div>
-		</React.Fragment>
-	);
-};
+			) : null;
+		}
+
+		return (
+			<React.Fragment>
+				{ this.props.data.image ? <Image data={this.props.data.image.fields}/> : null }
+				<H1 f2 fw6 lh_title mt0>
+					<Link route="post" params={{type: 'post', id: this.props.id, slug: slugify(this.props.data.title)}} passHref prefetch>
+						<A link near_black hover_blue>{quotes(widont(this.props.data.title))}</A>
+					</Link>
+				</H1>
+				<Div lh_copy pb3>
+					<ReactMarkdown
+						source={shouldSplitText ? splitText[0].trim() : this.props.data.body }
+						plugins={[shortcodes]}
+						escapeHtml={false}
+						renderers={{
+							shortcode: Embed,
+							image: BodyImage
+						}}
+					/>
+					{ this.state.showMore ? null : continueReading }
+					{ moreText }
+					{ this.state.showMore ? showLess : null }
+				</Div>
+			</React.Fragment>
+		);
+	};
+
+	toggleMore() {
+		this.setState(
+			prevState => ({showMore: !prevState.showMore})
+		);
+	}
+}
+
 Long.propTypes = {
 	data: PropTypes.object.isRequired,
 	id: PropTypes.string.isRequired
