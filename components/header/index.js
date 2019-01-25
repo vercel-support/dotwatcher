8e4d92dd68da1a58f56657837f5852af0826c904
Jon Heslop <jon@jonheslop.com>
2019-01-25 17:13:16 +0000

    Collapsing nav

--- components/header/index.js
+++ components/header/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import styled, {css} from 'styled-components';
 import tachyons from 'styled-components-tachyons';
 import slugify from 'slugify';
+import ResponsiveMenu from 'react-responsive-navbar';
 import {Link} from '../../routes';
 import Logo from './logo';
 
@@ -19,6 +20,7 @@ const Div = styled.div`
 	width: 120px;
 	margin-left: var(--spacing-large)
 `;
+const Button = styled.button`${tachyons}`
 
 class Banner extends Component {
 	constructor(props) {
@@ -94,36 +96,54 @@ class Banner extends Component {
 			${props => props.isFixed && css`
 				position: fixed;
 			`}
+			> div {
+				padding: var(--spacing-medium);
+				margin-left: auto;
+				line-height: 1;
+				font-size: 2.25rem;
+				display: flex;
+				align-items: center;
+				float: right;
+			}
 			${tachyons}`;
 		const padTop = this.state.fixed ? <Div style={{height: this.state.bannerHeight}}/> : null;
 		return (
 			<React.Fragment>
 				{padTop}
-				<Header bg_white bb b__light_gray near_black w_100 z_2 flex self_start className="cf" id="banner" isFixed={this.state.fixed}>
-					<H1 flex items_center f2 pv3 ma0 fw5 lh_solid w_40_l>
+				<Header bg_white bb b__light_gray near_black w_100 z_2 className="cf" id="banner" isFixed={this.state.fixed}>
+					<H1 fl flex items_center f2 pv3 ma0 fw5 lh_solid w_40_l>
 						<Div>
 							<Logo>{this.props.title}</Logo>
 						</Div>
 					</H1>
 					{
 						this.props.raceName ? (
-							<H2 dn pv3 ph4 flex_ns items_center f4 ma0 lh_solid fw5><Link route="race" params={{type: 'race', id: slugify(this.props.race.fields.title, {lower: true})}} passHref prefetch><A no_underline pt2 near_black hover_blue>{this.props.race.fields.title}</A></Link></H2>
+							<H2 fl dn pv3 ph4 flex_ns items_center f4 ma0 lh_solid fw5><Link route="race" params={{type: 'race', id: slugify(this.props.race.fields.title, {lower: true})}} passHref prefetch><A no_underline pt3 near_black hover_blue>{this.props.race.fields.title}</A></Link></H2>
 						) : null
 					}
-					<Nav ph4 pv3 lh_solid f2 flex_grow flex items_center>
-						<Link href="/races" as="/races" passHref prefetch>
-							<A dib pt2 mr3 f5 f4_l near_black hover_blue no_underline fw5>Races</A>
-						</Link>
-						<Link href="/results" as="/results" passHref prefetch>
-							<A dib pt2 mh3 f5 f4_l near_black hover_blue no_underline fw5>Results</A>
-						</Link>
-						<Link href="/features" as="/features" passHref prefetch>
-							<A dib pt2 mh3 f5 f4_l near_black hover_blue no_underline fw5>Features</A>
-						</Link>
-						<Link route="page" params={{type: 'page', id: '1BgGLGEpckYcmoEE6Cqc0I'}} passHref>
-							<A dib pt2 ml3 f5 f4_l near_black hover_blue no_underline fw5>About</A>
-						</Link>
-					</Nav>
+
+					<ResponsiveMenu 
+						menuOpenButton={<Button f5 pt2 fw5 bg_white tc near_black hover_blue bn>Menu</Button>}
+						changeMenuOn="800px"
+						largeMenuClassName=""
+						smallMenuClassName="small-menu-classname"
+						menu={
+							<React.Fragment>
+								<Link href="/races" as="/races" passHref prefetch>
+									<A dib pt2 mr3 f5 f4_l near_black hover_blue no_underline fw5>Races</A>
+								</Link>
+								<Link href="/results" as="/results" passHref prefetch>
+									<A dib pt2 mh3 f5 f4_l near_black hover_blue no_underline fw5>Results</A>
+								</Link>
+								<Link href="/features" as="/features" passHref prefetch>
+									<A dib pt2 mh3 f5 f4_l near_black hover_blue no_underline fw5>Features</A>
+								</Link>
+								<Link route="page" params={{ type: 'page', id: '1BgGLGEpckYcmoEE6Cqc0I' }} passHref>
+									<A dib pt2 ml3 f5 f4_l near_black hover_blue no_underline fw5>About</A>
+								</Link>
+							</React.Fragment>
+						}
+					/>
 				</Header>
 			</React.Fragment>
 		);
