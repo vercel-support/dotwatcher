fa1304f549bc6a7ac76966f75e95667dad9ff660
Jon Heslop <jon@jonheslop.com>
2019-02-01 16:04:02 +0000

    Fixing OG: titles

--- pages/results.js
+++ pages/results.js
@@ -26,7 +26,7 @@ class App extends Component {
 			<Page>
 				<Head>
 					<title>{ this.props.race } { this.props.year } results - DotWatcher.cc</title>
-					<meta property="og:title" content="{ this.props.race } { this.props.year } results - DotWatcher.cc"/>
+					<meta property="og:title" content={`${this.props.race} ${this.props.year} results - DotWatcher.cc`}/>
 					<meta property="og:description" content="DotWatcher is here to showcase the best of long distance self-supported bike racing."/>
 					<meta property="og:image" content="https://images.ctfassets.net/6hyijb95boju/KQ7Yj247Go6KOIm60SeQ2/9315aa310eee6a72088c9c37de8aa1e6/DotWatcher---Logo---Pin-_1_.jpg"/>
 				</Head>
