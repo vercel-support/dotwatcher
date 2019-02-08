8c5a88eb8ae9cefc0a53d6e71afb988791a8deb5
Jon Heslop <jon@jonheslop.com>
2019-02-08 17:13:44 +0000

    Even more metatags

--- pages/feature.js
+++ pages/feature.js
@@ -52,7 +52,7 @@ class FeaturePage extends React.Component {
 					<meta name="twitter:creator" content="@dotwatcher"/>
 					<meta name="twitter:title" content={`${this.props.feature.title} – DotWatcher.cc`} />
 					<meta name="twitter:description" content="DotWatcher is here to showcase the best of long distance self-supported bike racing." />
-					<meta name="twitter:image" content="https://a.ltrbxd.com/resized/sm/upload/m2/rx/cn/gv/the-favourite-1200-1200-675-675-crop-000000.jpg?k=461595f6f4" />
+					<meta name="twitter:image" content={this.props.feature.image ? `${this.props.feature.image.fields.file.url}` : 'https://images.ctfassets.net/6hyijb95boju/KQ7Yj247Go6KOIm60SeQ2/9315aa310eee6a72088c9c37de8aa1e6/DotWatcher---Logo---Pin-_1_.jpg' } />
 					<meta name="description" content={this.props.feature.excerpt ? this.props.feature.excerpt : 'DotWatcher is here to showcase the best of long distance self-supported bike racing.' } />
 					</Head>
 				<Header
--- pages/page.js
+++ pages/page.js
@@ -44,7 +44,14 @@ class ContentPage extends React.Component {
 					<title>{this.props.page.title} – DotWatcher.cc</title>
 					<meta property="og:title" content={`${this.props.page.title} – DotWatcher.cc`}/>
 					<meta property="og:description" content="DotWatcher is here to showcase the best of long distance self-supported bike racing."/>
-					<meta property="og:image" content="https://images.ctfassets.net/6hyijb95boju/KQ7Yj247Go6KOIm60SeQ2/9315aa310eee6a72088c9c37de8aa1e6/DotWatcher---Logo---Pin-_1_.jpg"/>
+					<meta property="og:image" content={this.props.page.image ? this.props.page.image.fields.file.url : 'https://images.ctfassets.net/6hyijb95boju/KQ7Yj247Go6KOIm60SeQ2/9315aa310eee6a72088c9c37de8aa1e6/DotWatcher---Logo---Pin-_1_.jpg'}/>
+					<meta name="twitter:card" content="summary_large_image" />
+					<meta name="twitter:site" content="@dotwatcher"/>
+					<meta name="twitter:creator" content="@dotwatcher"/>
+					<meta name="twitter:title" content={`${this.props.page.title} – DotWatcher.cc`}/>
+					<meta name="twitter:description" content="DotWatcher is here to showcase the best of long distance self-supported bike racing." />
+					<meta property="og:image" content={this.props.page.image ? this.props.page.image.fields.file.url : 'https://images.ctfassets.net/6hyijb95boju/KQ7Yj247Go6KOIm60SeQ2/9315aa310eee6a72088c9c37de8aa1e6/DotWatcher---Logo---Pin-_1_.jpg'}/>
+					<meta name="description" content="DotWatcher is here to showcase the best of long distance self-supported bike racing." />
 				</Head>
 				<Header
 					title="dotwatcher.cc"
