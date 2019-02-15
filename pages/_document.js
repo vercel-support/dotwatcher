306a126d98bdb8ec9f792a661939744363db4315
Jon Heslop <jon@jonheslop.com>
2019-02-15 16:15:01 +0000

    fix urls to icons

--- pages/_document.js
+++ pages/_document.js
@@ -19,13 +19,13 @@ export default class Document extends NextDocument {
 					<style dangerouslySetInnerHTML={{__html: stylesheet}}/>
 					<meta name="description" content="DotWatcher is here to showcase the best of long distance self-supported bike racing."/>
 					<meta name="viewport" content="initial-scale=1.0, width=device-width"/>
-					<link rel="icon" type="image/png" sizes="16x16" href="static/dw-pin-16.png"/>
-					<link rel="icon" type="image/png" sizes="32x32" href="static/dw-pin-32.png"/>
-					<link rel="icon" type="image/png" sizes="96x96" href="static/dw-pin-96.png"/>
-					<link rel="apple-touch-icon" href="static/dw-pin-120.png"/>
-					<link rel="apple-touch-icon" sizes="180x180" href="static/dw-pin-180.png"/>
-					<link rel="apple-touch-icon" sizes="152x152" href="static/dw-pin-152.png"/>
-					<link rel="apple-touch-icon" sizes="167x167" href="static/dw-pin-167.png"/>
+					<link rel="icon" type="image/png" sizes="16x16" href="/static/dw-pin-16.png"/>
+					<link rel="icon" type="image/png" sizes="32x32" href="/static/dw-pin-32.png"/>
+					<link rel="icon" type="image/png" sizes="96x96" href="/static/dw-pin-96.png"/>
+					<link rel="apple-touch-icon" href="/static/dw-pin-120.png"/>
+					<link rel="apple-touch-icon" sizes="180x180" href="/static/dw-pin-180.png"/>
+					<link rel="apple-touch-icon" sizes="152x152" href="/static/dw-pin-152.png"/>
+					<link rel="apple-touch-icon" sizes="167x167" href="/static/dw-pin-167.png"/>
 					<script src="//www.instagram.com/embed.js"/>
 				</Head>
 				<body>
