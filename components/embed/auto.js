07ee8aa456ca8c30cce77a17e8ef8c29820137a6
Jon Heslop <jon@jonheslop.com>
2019-04-12 12:56:03 +0100

    Add new markdown renderer that parses links and makes them into embeds
    Have made it work for:
    - youtube
    - twitter
    - instagram

---
+++ components/embed/auto.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Instagram from 'react-instagram-embed';
+import styled from 'styled-components';
+import tachyons from 'styled-components-tachyons';
+
+const tweetIdMatch = /status\/(\d+)/i
+const youtubeDomain = /youtu.?be./
+const youtubeIdMatch = /http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?[\w\?=]*)?/i
+const A = styled.a`${tachyons}`;
+
+class AutoEmbed extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inBrowser: false
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ inBrowser: true });
+  }
+
+  render() {
+    const tweetID = tweetIdMatch.exec(this.props.href);
+    const youtubeID = youtubeIdMatch.exec(this.props.href);
+
+    if (this.state.inBrowser && this.props.href.includes('twitter') && tweetID) {
+      const { TwitterTweetEmbed } = require('react-twitter-embed');
+      return <TwitterTweetEmbed tweetId={tweetID[1]} />;
+    }
+    if (this.state.inBrowser && this.props.href.includes('instagram.com')) {
+      return <Instagram url={this.props.href} />;
+    }
+    if (this.state.inBrowser && youtubeDomain.test(this.props.href)) {
+      return <iframe width="560" height="315" style={{ maxWidth: '100%' }} src={`https://www.youtube.com/embed/${youtubeID[1]}`} frameBorder="0" allowFullScreen />;
+    }
+    
+    return <A link blue href={this.props.href}>{this.props.href}</A>;
+  }
+}
+
+AutoEmbed.propTypes = {
+  href: PropTypes.string.isRequired,
+};
+
+export default AutoEmbed;
