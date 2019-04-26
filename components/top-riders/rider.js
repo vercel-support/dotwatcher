ad31f31089e354abcc45f422035e9a3cddfa8452
Jon Heslop <jon@jonheslop.com>
2019-04-26 11:04:16 +0100

    Link riders to our historic data on them

--- components/top-riders/rider.js
+++ components/top-riders/rider.js
@@ -2,18 +2,24 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import styled from 'styled-components';
 import tachyons from 'styled-components-tachyons';
+import { Link } from '../../routes';
 
 const Wrap = styled.dl`${tachyons}`;
 const RiderName = styled.dt`${tachyons}`;
 const Name = styled.span`${tachyons}`;
 const RiderStat = styled.dt`${tachyons}`;
+const A = styled.a`${tachyons}`;
 
 const Rider = ({rider, numbered, position}) => {
 	return (
 		<Wrap f6 mt0 mb2 lh_copy className="cf">
 			<RiderName fl f6>
 				{ numbered ? `${position}. ` : null }
-				<Name fw6>{rider.name}</Name>
+				<Link route="profile" params={{ type: 'profile', name: rider.name }} passHref>
+					<A link near_black hover_blue underline title={`See ${rider.name}’s past results`}>
+						<Name fw6>{rider.name}</Name>
+					</A>
+				</Link>
 			</RiderName>
 			{
 				rider.distance ? <RiderStat fr f6 gray>{rider.distance}km</RiderStat> : null
